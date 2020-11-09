edb69654be42f85e106f7640c440fbd0314536b3
MiluVol <74131499+MiluVol@users.noreply.github.com>
2020-11-10 00:07:19 +0300

    Add files via upload

---
+++ tablitsa.py
@@ -0,0 +1,9 @@
+a = input('Escape sequences\n'
+      '\\a\t\tBell (alert)\n'
+      '\\b\t\tBackspace\n'
+      '\\n\t\tNew line\n'
+      '\\t\t\tHorizontal tab\n'
+      '\\\ \t\tBackslash \\\n'
+      '\\"\t\tDouble quotation mark "\n'
+      '\\\'\t\tSingle quotation mark \'')
+print(a)
